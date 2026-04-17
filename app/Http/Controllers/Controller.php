4e58937cde3735516c7596d119a507be03c553e4
Michael Mallmann <michaelmallmann07@gmail.com>
2026-04-17 20:20:33 -0300

    fix: cleanup base controller and remove class redeclaration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,16 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Invoice; 
-use Illuminate\Http\Request;
-
-class InvoiceController extends Controller
+abstract class Controller
 {
-    public function store(Request $request)
-    {
-        
-        Invoice::create($request->all());
 
-        return redirect()->back()->with('success', 'Nota processada com sucesso!');
-    }
 }
